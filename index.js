43377e32672b4aa1d736a8362ed6d4b1003bda29
201steve <jeongillee201@gmail.com>
2022-04-10 16:07:25 +0900

    wip

--- index.js
+++ index.js
@@ -2,16 +2,35 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//application / x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
 
 mongoose
   .connect(
-    "mongodb+srv://jeongil:dlwjddlf100@toyproject.ytjv2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    `mongodb+srv://${process.env.USER_KEY}:${process.env.USER}@toyproject.ytjv2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
   )
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  //회원가입 필요 정보들을 client에서 받으면 데이터베이스에 넣어준다.
+
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
